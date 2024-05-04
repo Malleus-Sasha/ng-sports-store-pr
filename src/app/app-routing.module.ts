ac484ec3ec6b6a66e3ca8aecded203e9238bcba3
Alex Mas <am@g>
2024-05-04 19:07:30 +0300

    STS: Added Store router guard first

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,12 @@ import { NotFoundPageComponent } from './components/not-found-page/not-found-pag
 import { CartDetailComponent } from './store/cart-detail/cart-detail.component';
 import { CheckoutComponent } from './store/checkout/checkout.component';
 import { StoreComponent } from './store/store/store.component';
+import { storeFirstGuard } from './guards/store-first.guard';
 
 const routes: Routes = [
-  { path: 'store', component: StoreComponent },
-  { path: 'cart', component: CartDetailComponent },
-  { path: 'checkout', component: CheckoutComponent },
+  { path: 'store', component: StoreComponent, canActivate: [storeFirstGuard] },
+  { path: 'cart', component: CartDetailComponent, canActivate: [storeFirstGuard] },
+  { path: 'checkout', component: CheckoutComponent, canActivate: [storeFirstGuard] },
   { path: 'pageNotFound', component: NotFoundPageComponent },
   { path: '**', redirectTo: 'pageNotFound'}
 ];
---
+++ src/app/guards/store-first.guard.ts
@@ -0,0 +1,17 @@
+import { CanActivateFn, Router } from '@angular/router';
+import { StoreComponent } from '../store/store/store.component';
+import { inject } from '@angular/core';
+
+export const storeFirstGuard: CanActivateFn = (route, state) => {
+  const router = inject(Router);
+  let firstNavigation = true;
+
+  if (firstNavigation) {
+    firstNavigation = false;
+    if (route.component != StoreComponent) {
+      router.navigateByUrl("/");
+      return false;
+    }
+  }
+  return true;
+};
