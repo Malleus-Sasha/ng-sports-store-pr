fe929ea0be572b9fd9a410519d42b7aec15e4237
Alex Mas <am@g>
2024-05-09 16:56:38 +0300

    STS: Added Product Editor

--- src/app/admin/product-editor/product-editor.component.ts
+++ src/app/admin/product-editor/product-editor.component.ts
@@ -1,18 +1,39 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { ProductRepository } from "../../model/product.repository";
+import { Product } from "../../model/product.model";
+import { ActivatedRoute, Router } from "@angular/router";
+import { NgForm } from "@angular/forms";
 
 @Component({
-  selector: 'app-product-editor',
-  template: `
-    <p>
-      product-editor works!
-    </p>
-  `,
+  selector: "app-product-editor",
+  templateUrl: "product-editor.component.html",
   styles: `
     :host {
       display: block;
     }
-  `
+  `,
 })
 export class ProductEditorComponent {
+  editing: boolean = false;
+  product: Product = new Product(0);
 
+  constructor(
+    private repository: ProductRepository,
+    private router: Router,
+    activeRoute: ActivatedRoute
+  ) {
+    this.editing = activeRoute.snapshot.params["mode"] == "edit";
+    console.log(':SHNAP:', activeRoute.snapshot);
+    if (this.editing) {
+      Object.assign(
+        this.product,
+        repository.getProduct(activeRoute.snapshot.params["id"])
+      );
+    }
+  }
+
+  save(form: NgForm) {
+    this.repository.saveProduct(this.product);
+    this.router.navigateByUrl("/admin/main/products");
+  }
 }
--- src/app/admin/product-table/product-table.component.ts
+++ src/app/admin/product-table/product-table.component.ts
@@ -1,18 +1,24 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { Product } from "../../model/product.model";
+import { ProductRepository } from "../../model/product.repository";
 
 @Component({
-  selector: 'app-product-table',
-  template: `
-    <p>
-      product-table works!
-    </p>
-  `,
+  selector: "app-product-table",
+  templateUrl: "product-table.component.html",
   styles: `
     :host {
       display: block;
     }
-  `
+  `,
 })
 export class ProductTableComponent {
+  constructor(private repository: ProductRepository) {}
 
+  getProducts(): Product[] {
+    return this.repository.getProducts('');
+  }
+
+  deleteProduct(id: number) {
+    this.repository.deleteProduct(id);
+  }
 }
